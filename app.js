930428a6faff29d4c47c7d01cd2360a3a81898a6
--unset-all <--unset-all>
2021-02-17 17:18:36 -0600

    agrego workers & bull / bull-board

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const { setQueues,
+   BullAdapter, router } = require('bull-board');
+const Queue = require('./lib/Queue');
+
 
 //SE CREAL EL SERIVODR
 const app = express();
+setQueues(Queue.queues.map(queue => new BullAdapter(queue.bull)))
+
 
 //HABILITAR EXPRESS.JSON
 app.use(express.json({ extended: true }));
@@ -17,10 +23,15 @@ app.set('port', process.env.PORT || 4000);
 //AGREGO VISTA PARA INCIO
 app.use(express.static(path.join(__dirname, 'public')));
 
-//IMPORTAR USUARIOS
+//WORKER DASHBOARD BULL
+app.use('/admin/queues', router);
+
+//IMPORTAR USUARIOSd
 app.use('/api/usuarios', require('./routes/usuariosRoute'));
 app.use('/api/auth', require('./routes/authRoute'));
 app.use('/api/proyectos', require('./routes/proyectosRoute'));
 app.use('/api/tareas', require('./routes/tareasRoute'));
+app.use('/api/send-email', require('./routes/emailRoute'));
+
 
 module.exports = app;
---
+++ queue.js
@@ -0,0 +1,5 @@
+require('dotenv').config({ path: 'variables.env' });
+
+const Queue = require('./lib/Queue');
+
+Queue.process();
